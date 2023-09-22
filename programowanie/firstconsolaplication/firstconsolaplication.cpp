da46e50c7ca5bd2801164ce9304efc1ffbfc1f64
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-22 11:50:55 +0200

    fd

--- programowanie/firstconsolaplication/firstconsolaplication.cpp
+++ programowanie/firstconsolaplication/firstconsolaplication.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello   Worl d!\n";
-}
+    std::cout << "Hello   World!\n";
+    std::cout << -59.5 << "dasdasda" << 'x' << true << false << '\n';
+    int number;
+    int number = 5;
 
+    int secondnumber , x;
+    x =  secondnumber = number + 2 ;
+}
