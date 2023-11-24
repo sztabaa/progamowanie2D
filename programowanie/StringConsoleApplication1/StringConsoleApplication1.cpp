5ba0509bdafccf4c9f4fa717f29bf8d782282732
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-11-24 13:19:13 +0100

    cdsd

--- programowanie/StringConsoleApplication1/StringConsoleApplication1.cpp
+++ programowanie/StringConsoleApplication1/StringConsoleApplication1.cpp
@@ -1,3 +1,14 @@
+// Napisz program, który bêdzie prosi³ o has³o. Nie przepuœci dalej dopóki nie zostanie ono podane prawid³owo.
+// Napisz program, który pobiera od u¿ytkownika ci¹g znaków i wyœwietla liczbê samog³osek i spó³g³osek w tym ci¹gu.
+//* Poproœ u¿ytkownika o wprowadzenie liczby ca³kowitej w systemie dziesiêtnym.Nastêpnie skonwertuj tê liczbê na system dwójkowy(binarny) i wyœwietl wynik.
+//* Program sprawdzaj¹cy czy podany ci¹g znaków jest palindromem(czyli takim, który czytany od ty³u jest taki sam, jak czytany od przodu, np. "kajak")
+//* Program sprawdzaj¹cy czy podane dwa s³owa s¹ anagramami(czyli takimi, które zawieraj¹ te same litery, ale w innym uk³adzie, np. "klasa" i "salka")
+//*
+//
+//* Program wyci¹gaj¹cy informacje z numeru PESEL
+//* Program implementuj¹cy algorytm szyfrowania Cezara(proste szyfrowanie, w którym ka¿dy znak w tekœcie jest zastêpowany innym znakiem, przesuniêtym o sta³¹ liczbê pozycji w alfabecie).
+
+
 #include <iostream>
 using namespace std;
 
@@ -34,13 +45,38 @@ void task2()
 	cout << "podaj swoje imie\n";
 	cin >> userName;
 
-	cout << "No hej  " << userName << "\n";
+	cout << "No hej   " << userName << "\n";
+}
+//	Napisz program, który bêdzie prosi³ o has³o.Nie przepuœci dalej dopóki nie zostanie ono podane prawid³owo.
+void task3()
+{
+	string createPassword;
+	string password;
+	cout << "utwurz haslo \n";
+	cin >> createPassword;
+
+	cout << "zaloguj sie\n";
+	cin >> password;
+
+	do
+	{
+		if (password == createPassword)
+		{
+			cout << "haslo jest poprawne";
+		}
+		else
+		{
+			cout << "zle!!!";
+		}
+
+	} while (password != createPassword);
 }
 
 int main()
 {
 	//task1();
-	task2();
+	//task2();
+	task3();
 }
 
 
