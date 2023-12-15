87aa1aaf6c9532bdf8ca70bee7f8e6d1fe1088b8
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-12-15 13:13:51 +0100

    fdssdg

--- programowanie/ArrayConsoleApplication1/ArrayConsoleApplication1.cpp
+++ programowanie/ArrayConsoleApplication1/ArrayConsoleApplication1.cpp
@@ -2,18 +2,22 @@ using namespace std;
 #include <iostream>
 void task1()
 {
+    const int UPPER_RANGE = 10;
+    const int LOWER_RANGE = 5;
+
     const int SIZE_OF_ARRAY = 5;
     int numbers[SIZE_OF_ARRAY];
 
+    srand(time(NULL));
+
     for (int i = 0; i < SIZE_OF_ARRAY; i++)
     {
-        cout << "podaj liczbe\n";
-        cin >> numbers[i];
+        numbers[i] = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
     }
 
     for (int i = SIZE_OF_ARRAY - 1; i >= 0; i--)
     {
-        cout << "Podana liczba to:    " << numbers[i] << "\n";
+        cout << numbers[i] << ", ";
     }
 }
 
