297f7b3f52caab8b76e53a667a6a5f058d3f7113
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-12-18 11:17:57 +0100

    tdfghdfghhf

--- programowanie/ArrayConsoleApplication1/ArrayConsoleApplication1.cpp
+++ programowanie/ArrayConsoleApplication1/ArrayConsoleApplication1.cpp
@@ -19,6 +19,28 @@ void task1()
     {
         cout << numbers[i] << ", ";
     }
+
+    //szukanie maksimum
+    int max = numbers[0];
+    for (int i = 1; i < SIZE_OF_ARRAY; i++)
+    {
+        if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    }
+
+    //szukanie minimum
+    int min = numbers[0];
+    for (int i = 1; i < SIZE_OF_ARRAY; i++)
+    {
+        if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+    }
+    cout << "Najwieksza liczba to:   " << max << "\n";
+    cout << "Najmniejsza liczba to:   " << min << "\n";
 }
 
 int main()
