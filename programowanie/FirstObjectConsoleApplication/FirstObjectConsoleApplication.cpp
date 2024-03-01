9668735438ea46318bda3c12227fc98ef54f8f42
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-03-01 12:45:50 +0100

    5ytyyyu

--- programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
+++ programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -2,30 +2,58 @@
 #include <iostream>
 using namespace std;
 
-struct Point
+class Point
 {
+private:
 	int x;
 	int y;
 	int z;
+
+
+
+public:
+	void setX(int value)
+	{
+		//x=abs(value);
+		if (value >= 0)
+		{
+			x = value;
+		}
+		else
+		{
+			//reakcja na b³¹d
+		}
+	}
+
+	int getX()
+	{
+		//if (uprawnienia)
+
+		return x;
+	}
+
+	double distanceFromCenter()
+	{
+		double distance;
+		distance = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+		return distance;
+	}
+
+	
 };
 
-double distanceFromCenter(point p)
-{
-	double distance;
-	distance = sqrt(pow(p.x, 2) + pow(p.y, 2) + pow(p.z, 2));
-	return distance;
-}
 
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
 
 	Point firstPoint;
-	firstPoint.x = 3;
-	firstPoint.y = 5;
-	firstPoint.z = 8;
+	firstPoint.setX(-3);
+	//firstPoint.x = 3;
+	//firstPoint.y = 5;
+	//firstPoint.z = 8;
 
-	cout << "Odleg³oœæ:  " << distanceFromCenter(firstPoint) << "\n";
+	cout << "Odleg³oœæ:  " << firstPoint.distanceFromCenter() << "\n";
 
 
 }
