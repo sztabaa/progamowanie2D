344deb2cb5079ce0a44d67932217a8fa22c7547e
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-03-04 11:32:55 +0100

    dsa

--- programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
+++ programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -12,6 +12,32 @@ private:
 
 
 public:
+	Point()
+	{
+		x = 3;
+		y = 7;
+		z = 10;
+	 }
+
+	Point(int xx)
+	{
+		x = xx;
+		y = 7;
+		z = 10;
+	}
+
+	Point(int xx , int yy , int zz)
+	{
+		x = xx;
+		y = yy;
+		z = zz;
+	}
+
+	~Point()
+	{
+		cout << "Teraz dzia³a destruktor \n";
+	}
+
 	void setX(int value)
 	{
 		//x=abs(value);
@@ -42,19 +68,72 @@ public:
 	
 };
 
+class Rectrangle
+{
+private:
+	 int firstSide;
+	 int secondSide;
+	 int thirdSide;
+	 int fourthSide;
+public:
+	Rectrangle()
+	{
+		firstSide = 3;
+		secondSide = 3;
+		thirdSide = 5;
+		fourthSide = 5;
+	}
+ 
+	Rectrangle(int side1, int side2)
+	{
+		firstSide = secondSide = side1;
+		thirdSide = fourthSide = side2;
+	}
+
+	double AreaOfRectrangle()
+	{
+		double area;
+		area = firstSide * thirdSide;
+		return area;
+
+	 }
+
+	double CircutOfRectrangle()
+	{
+		double Circut;
+		Circut = 2 * firstSide + 2 * thirdSide;
+		return Circut;
+	}
+
+	void  InformationAboutRectrangle()
+	{
+		cout << "obwód: " << CircutOfRectrangle() << "\n";
+		cout << "pole: " << AreaOfRectrangle() << "\n";
+		
+	 }
+
+};
+
 
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
-
-	Point firstPoint;
-	firstPoint.setX(-3);
-	//firstPoint.x = 3;
-	//firstPoint.y = 5;
-	//firstPoint.z = 8;
-
-	cout << "Odleg³oœæ:  " << firstPoint.distanceFromCenter() << "\n";
+	//{
+	//	Point firstPoint;
+	//	firstPoint.setX(-3);
+	//	//firstPoint.x = 3;
+	//	//firstPoint.y = 5;
+	//	//firstPoint.z = 8;
 
 
+
+	//	cout << "Odleg³oœæ:  " << firstPoint.distanceFromCenter() << "\n";
+	//}
+
+	//Point secondPoint(15);
+	//cout << "Odleg³oœæ: " << secondPoint.distanceFromCenter() << "\n";
+	Rectrangle firstRectrangle;
+
+	firstRectrangle.InformationAboutRectrangle();
 }
 
