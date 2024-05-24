06d912dbccc3b97b5c79e30a838de2856d4555c0
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-05-24 12:36:09 +0200

    dfsfas

--- programowanie/ConsoleApplication1/ConsoleApplication1.cpp
+++ programowanie/ConsoleApplication1/ConsoleApplication1.cpp
@@ -103,7 +103,7 @@ using namespace std;
 //};
 
 class Osoba {
-private:
+protected:
 	string firstName;
 	string lastName;
 	int age;
@@ -137,17 +137,53 @@ Osoba createPerson()
 	cin >> age;
 	Osoba informacje(firstName, lastName, age);
 	return informacje;
-}
+};
 
 class Ksi¹¿ka : public Osoba
 {
 private:
 	string tytul;
-	Osoba autor;
-	
-
+	string data;
 public:
+	Ksi¹¿ka(string firstName, string lastName, int age, string title, string date) : Osoba(firstName, lastName, age)
+	{
+		tytul = title;
+		data = date;
+	}
+
+	void Wypisz()
+	{
+		cout << "imie autora:" << firstName << "\n";
+		cout << "nazwisko autora: " << lastName << "\n";
+		cout << "wiek autora: " << age << "\n";
+		cout << "tytu³ ksi¹¿ki: " << tytul << "\n";
+		cout << "data wydania ksi¹¿ki: " << data << "\n";
+	}
 };
+
+Ksi¹¿ka createBook()
+{
+	string firstName;
+	string lastName;
+	int age;
+	string tytul;
+	string data;
+
+
+	cout << "podaj imie: ";
+	cin >> firstName;
+	cout << "podaj nazwisko: ";
+	cin >> lastName;
+	cout << "podaj wiek: ";
+	cin >> age;
+	cout << "podaj tytu³ ksi¹¿ki: ";
+	cin >> tytul;
+	cout << "podaj date wydania ksi¹¿ki: ";
+	cin >> data;
+
+	Ksi¹¿ka ksiêga(firstName, lastName, age, tytul, data);
+	return ksiêga;
+}
 int main()
 {
 	/*Licz obliczenia(10);
@@ -159,6 +195,8 @@ int main()
 	Osoba newPerson = createPerson();
 	newPerson.wypisz();
 
+	Ksi¹¿ka newBook = createBook();
+	newBook.Wypisz();
 
 }
 
