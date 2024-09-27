0a97ba510a55d29743f8ab0b12ac119885a346ea
q <q>
2024-09-27 11:38:07 +0200

    sdfsf

--- programowanie/StructConsoleApp/Program.cs
+++ programowanie/StructConsoleApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Point firstPoint = new Point();
+firstPoint.x = 3;
+firstPoint.y = 10;
+Point secondPoint = firstPoint;
+secondPoint.y = 6;
+secondPoint.y = 99; 
+Console.WriteLine(firstPoint.x);
+Console.WriteLine(secondPoint.x);
