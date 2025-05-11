c00f3082b43a7853e569d0ab31f69cd23fde8a18
sztabaa <144441233+sztabaa@users.noreply.github.com>
2025-05-11 23:49:44 +0200

    Add files via upload

---
+++ matura2022czerwiecConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿StreamReader streamReader = new StreamReader("przyklad.txt");
+
+string strNumberFromFile;
+List<string> strNumbers = new List<string>();
+
+while ((strNumberFromFile = streamReader.ReadLine()) != null)
+{
+    strNumbers.Add(strNumberFromFile);
+}
+
+streamReader.Close();
+
+//Zadanie 4.1
+var reverseDovidedBy17 = strNumbers
+    .Where(s => int.Parse(string.Join("", s.Reverse())) % 17 == 0)
+    .Select(s => string.Join("", s.Reverse()));
+
+Console.WriteLine("Zdanie 4.1:");
+foreach (var strNumber in reverseDovidedBy17)
+    Console.WriteLine(strNumber);
+
+//Zadanie 4.2
+Console.WriteLine("Zdanie 4.2:");
+
+string strMaxNumber = "";
+int maxDiff = int.MinValue;
+foreach (var strNumber in strNumbers)
+{
+    int dif = Math.Abs(int.Parse(strNumber) - int.Parse(string.Join("", strNumber.Reverse())));
+    if (dif > maxDiff)
+    {
+        maxDiff = dif;
+        strMaxNumber = strNumber;
+    }
+}
+
+Console.WriteLine($"{strMaxNumber} {maxDiff}");
+
+strMaxNumber = strNumbers.OrderBy(s => Math.Abs(int.Parse(s) - int.Parse(string.Join("", s.Reverse())))).Last();
+maxDiff = Math.Abs(int.Parse(strMaxNumber) - int.Parse(string.Join("", strMaxNumber.Reverse())));
+
+Console.WriteLine($"{strMaxNumber} {maxDiff}");
